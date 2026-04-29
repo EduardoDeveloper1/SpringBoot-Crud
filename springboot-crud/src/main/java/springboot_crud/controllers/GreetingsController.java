bb451ef225a22e57112d81f9a4ae7847d9677320
Eduardo <eduardodeveloper1@gmail.com>
2026-04-29 12:33:52 -0300

    Criado os endpoints para salvar, deletar e listar todos os usuario

--- springboot-crud/src/main/java/springboot_crud/controllers/GreetingsController.java
+++ springboot-crud/src/main/java/springboot_crud/controllers/GreetingsController.java
@@ -3,9 +3,15 @@ package springboot_crud.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import springboot_crud.Repository.UsuarioRepository;
@@ -43,5 +49,21 @@ public class GreetingsController {
 		List<Usuario> usuarios = usuarioRepository.findAll();
 
 		return ResponseEntity.ok(usuarios);
+	}
+
+	@PostMapping(value = "salvar") // Mapeia a URL
+	@ResponseBody // Descrição da resposta
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) { // Recebe os dados parasalvar
+
+		Usuario user = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(value = "delete") // Mapeia a URL
+	@ResponseBody // Descrição da resposta
+	public ResponseEntity<String> deletar(@RequestParam Long id) { // Recebe os dados parasalvar
+
+		usuarioRepository.deleteById(id);
+		return new ResponseEntity<String>("Usuario deletado com sucesso!", HttpStatus.OK);
 	}
 }
